7d2b2dedf362053243b26c80a7151f938841d547
Aleksandr Trostyanko <ce-aleksandr.trostyanko@beyondnow.com>
2023-10-19 17:11:02 +0200

    Added First App

---
+++ FirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApp // Note: actual namespace depends on the project name.
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
