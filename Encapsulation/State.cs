1c14a4c4baf625a8123033481d681c13a0f17d7d
Aleksandr Trostyanko <atrostynko@gmail.com>
2023-12-02 13:01:29 +0100

    Updated

--- Encapsulation/State.cs
+++ Encapsulation/State.cs
@@ -8,8 +8,8 @@ class State
     // поле доступно только из текущего класса
     private string privateVar = "private";
 
-    // доступно из текущего класса и производных классов, которые определены в этом же проекте
-    protected private string protectedPrivateVar = "protected private";
+    // доступно в любом месте программы, а также для других программ и сборок
+    public string publicVar = "public";
 
     // доступно из текущего класса и производных классов
     protected string protectedVar = "protected";
@@ -20,27 +20,26 @@ class State
     // доступно в любом месте текущего проекта и из классов-наследников в других проектах
     protected internal string protectedInternalVar = "protected internal";
 
-    // доступно в любом месте программы, а также для других программ и сборок
-    public string publicVar = "public";
+    // доступно из текущего класса и производных классов, которые определены в этом же проекте
+    private protected string protectedPrivateVar = "protected private";
 
-    // по умолчанию имеет модификатор private
-    void Print() => Console.WriteLine(defaultVar);
-
-    // метод доступен только из текущего класса
-    private void PrintPrivate() => Console.WriteLine(privateVar);
-
-    // доступен из текущего класса и производных классов, которые определены в этом же проекте
-    protected private void PrintProtectedPrivate() => Console.WriteLine(protectedPrivateVar);
-
-    // доступен из текущего класса и производных классов
-    protected void PrintProtected() => Console.WriteLine(protectedVar);
-
-    // доступен в любом месте текущего проекта
-    internal void PrintInternal() => Console.WriteLine(internalVar);
-
-    // доступен в любом месте текущего проекта и из классов-наследников в других проектах
-    protected internal void PrintProtectedInternal() => Console.WriteLine(protectedInternalVar);
 
     // доступен в любом месте программы, а также для других программ и сборок
     public void PrintPublic() => Console.WriteLine(publicVar);
+
+    // по умолчанию имеет модификатор private
+    void Print() => Console.WriteLine(defaultVar);
+    
+    // доступен в любом месте текущего проекта
+    internal void PrintInternal() => Console.WriteLine(internalVar);
+    
+    // доступен из текущего класса и производных классов
+    protected void PrintProtected() => Console.WriteLine(protectedVar);
+    
+    // доступен в любом месте текущего проекта и из классов-наследников в других проектах
+    protected internal void PrintProtectedInternal() => Console.WriteLine(protectedInternalVar);
+    
+    // доступен из текущего класса и производных классов, которые определены в этом же проекте
+    protected private void PrintProtectedPrivate() => Console.WriteLine(protectedPrivateVar);
+
 }
