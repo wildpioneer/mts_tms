d77764a4aed4cce4865bcc2e026680a04b17f65c
Aleksandr Trostyanko <atrostynko@gmail.com>
2023-12-14 13:13:43 +0100

    Updated

--- Collections/IList/ListExample.cs
+++ Collections/IList/ListExample.cs
@@ -8,14 +8,19 @@ public class ListExample
     {
         // Создаем список строк
         List<string> stringList = new List<string>();
+        List<string> stringList1 = new List<string>(16);
+        Console.WriteLine($"Capacity1 : {stringList.Capacity}");
+        Console.WriteLine($"Capacity2 : {stringList1.Capacity}");
 
         // Добавление элементов
         stringList.Add("Первый");
         stringList.Add("Второй");
         stringList.Add("Третий");
+        stringList.AddRange(new string[] { "Test", "Test1", "Test2" });
 
         // Вставка элемента по индексу
         stringList.Insert(1, "Новый Второй");
+        stringList.InsertRange(stringList.Count, new List<string>() { "Test4", "Test5" });
 
         // Вывод всех элементов
         Console.WriteLine("Элементы в списке:");
@@ -25,7 +30,7 @@ public class ListExample
         string elementAtIndex = stringList[2];
         Console.WriteLine($"Элемент по индексу 2: {elementAtIndex}");
 
-        // Поиск элемента
+        // Проверка содержания элемента
         bool containsElement = stringList.Contains("Третий");
         Console.WriteLine($"Список содержит 'Третий': {containsElement}");
 
@@ -43,21 +48,39 @@ public class ListExample
         var index = stringList.IndexOf("Новый Второй");
         Console.WriteLine($"Поиск индекса 'Новый Второй': {index}");
         
-        // Удаление элемента
-        stringList.Remove("Второй");
-
-        // Удаление элемента по индексу
-        stringList.RemoveAt(0);
-
-        // Вывод после удаления
-        Console.WriteLine("Элементы после удаления:");
-        PrintHelper.PrintCollection(stringList);
-
         // Reverse элементов
         stringList.Reverse();
         Console.WriteLine("Перевернутая коллекция:");
         PrintHelper.PrintCollection(stringList);
 
+        // Получение диапазона и копирование в массив
+        List<string> range = stringList.GetRange(0, 2);
+        Console.WriteLine("Часть коллекции:");
+        PrintHelper.PrintCollection(range);
+
+        string[] part = new string[3];
+        stringList.CopyTo(0, part, 1, 2);
+        Console.WriteLine("Копия коллекции:");
+        PrintHelper.PrintCollection(part);
+
+        // Пример ForEach
+        stringList.ForEach(p => Console.WriteLine(p));
+
+        // Удаление элемента по индексу
+        stringList.RemoveAt(0);
+
+        // Удаление элемента по значению
+        bool result = stringList.Remove("Третий");
+        Console.WriteLine($"Удаление элемента 'Третий': {result}");
+
+        // Удаление группы элементов
+        PrintHelper.PrintCollection(stringList);
+        stringList.RemoveRange(0, 1);
+
+        // Вывод после удаления
+        Console.WriteLine("Элементы после удаления:");
+        PrintHelper.PrintCollection(stringList);
+        
         // Очистка
         stringList.Clear();
         Console.WriteLine($"Количество элементов после очистки: {stringList.Count}");
