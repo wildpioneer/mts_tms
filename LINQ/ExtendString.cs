c0f0ce9cc9fd4c1695280b58566b7c8aa247ee4c
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-01-22 17:21:36 +0100

    Updated

--- LINQ/ExtendString.cs
+++
@@ -1,9 +0,0 @@
-namespace LINQ;
-
-public static class ExtendString
-{
-    public static char GetChar(this string value, int index)
-    {
-        return value[index];
-    }
-}
--- NUnitProject/PrePostConditionExample.cs
+++ NUnitProject/PrePostConditionExample.cs
@@ -2,17 +2,11 @@ namespace NUnitTest;
 
 public class PrePostConditionExample
 {
-    #region One Time Precondition Section
-    /// <summary>
-    /// Initializes Global test objects
-    /// </summary>
-    /// 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
         Console.WriteLine($"{this}: OneTimeSetUp...");
     }
-    #endregion
     
     [SetUp]
     public void Setup()
