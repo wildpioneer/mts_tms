28ca854c64032afeb2f9b00fc672059b4531c657
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-02-18 14:07:03 +0100

    Updated

--- Wrappers/Wrappers/Input.cs
+++ Wrappers/Wrappers/Input.cs
@@ -1,4 +1,5 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace Wrappers.Wrappers
 {
@@ -9,6 +10,8 @@ namespace Wrappers.Wrappers
         public Input(IWebDriver? driver, By @by)
         {
             _uiElement = new UIElement(driver, @by);
+            
+            //SelectElement 
         }
 
         public void Click() => _uiElement.Click();
