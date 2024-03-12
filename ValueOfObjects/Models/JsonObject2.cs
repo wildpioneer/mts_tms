1370fd1115fe570645cf035ab9e371d610f06fba
Aleksandr Trostyanko <atrostynko@gmail.com>
2024-03-12 12:35:56 +0100

    Updated

--- ValueOfObjects/Models/JsonObject2.cs
+++ ValueOfObjects/Models/JsonObject2.cs
@@ -1,9 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace ValueOfObjects.Models;
 
 public class JsonObject2
 {
     public string Name { get; set; }
-    public int FieldValue { get; set; }
+    [JsonPropertyName("value")] public int FieldValue { get; set; }
     public bool IsEnabled { get; set; }
-    public object IgnoreObject { get; set; }
+    [JsonIgnore] public object IgnoreObject { get; set; }
+
+    public override string ToString()
+    {
+        return
+            $"{nameof(Name)}: {Name}, {nameof(FieldValue)}: {FieldValue}, {nameof(IsEnabled)}: {IsEnabled}, {nameof(IgnoreObject)}: {IgnoreObject}";
+    }
 }
--- ValueOfObjects/Tests/JsonTest.cs
+++ ValueOfObjects/Tests/JsonTest.cs
@@ -22,13 +22,45 @@ public class JsonTest
     }
 
     [Test]
-    public void JsonDeserializerTest()
+    public void JsonDeserializerTest1()
     {
         string json = "{\"Name\":\"Test Name\",\"FieldValue\":10,\"IsEnabled\":true,\"IgnoreObject\":\"Test Object\"}";
         var jsonObject1 = JsonSerializer.Deserialize<JsonObject1>(json);
 
         Console.WriteLine(jsonObject1);
     }
+
+    [Test]
+    public void JsonDeserializerTest2()
+    {
+        using FileStream fs = new FileStream(@"Resources/testObject1.json", FileMode.Open);
+        var jsonObject1 = JsonSerializer.Deserialize<JsonObject1>(fs);
+
+        Console.WriteLine(jsonObject1);
+    }
     
-    
+    [Test]
+    public void JsonSerializerTest1()
+    {
+        var testObject1 = new JsonObject2
+        {
+            Name = "Test Name",
+            FieldValue = 10,
+            IsEnabled = false,
+            IgnoreObject = new string("Test Object")
+        };
+
+        var jsonString = JsonSerializer.Serialize<JsonObject2>(testObject1);
+
+        Console.WriteLine(jsonString);
+    }
+
+    [Test]
+    public void JsonDeserializerTest3()
+    {
+        using FileStream fs = new FileStream(@"Resources/testObject2.json", FileMode.Open);
+        var objectsList = JsonSerializer.Deserialize<List<JsonObject2>>(fs);
+
+        objectsList!.ForEach(Console.WriteLine);
+    }
 }
